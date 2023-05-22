144e15e99c0eb26c00a61a495df66115b04c9409
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-05-21 22:09:16 -0500

    Se crea la ruta y los metodos/funciones para enviar informacón a la DB

--- index.php
+++ index.php
@@ -15,6 +15,26 @@ Flight::route('GET /', function () {
 
 });
 
+//Envio de informacion a la DB
+Flight::route('POST /', function () {
+    //Declaramos los nombre con nombres de columnas en este caso fueron "Name" y "surname"
+    $Name=(Flight::request()->data->Name);
+    $surname=(Flight::request()->data->surname);
+    
+    //Introducimos la instruccion SQL para agregar a la tabla
+    $sql="INSERT INTO `estudiantes` (Name, surname) VALUES (?,?);";
+    $sentence=Flight::db()->prepare($sql);
+    //Pasamos como parametro a la DB los valores a insertar
+    $sentence->bindParam(1,$Name);
+    $sentence->bindParam(2,$surname);
+
+    //Se ejecuta la funcion de envio
+    $sentence->execute();
+
+    //y si todo salio bien se mostrara este mensaje
+    Flight::json(["Alumno Agregado con Exito"]);
+
+});
 
 
 
