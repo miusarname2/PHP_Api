c02133ca17a0b3b969409f119f245c418ab583f0
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-05-21 21:42:04 -0500

    Se crea la ruta para la obtención de información de la DB

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
-
 require 'flight/Flight.php';
 
-Flight::route('/', function () {
-    echo 'hello world!';
+// Conexion base de datos
+Flight::register('db', 'PDO', array('mysql:host=localhost:49674;dbname=api','root',''));
+
+//Retorna toda la data de la DB
+Flight::route('GET /', function () {
+    
+    $sentence=Flight::db()->prepare("SELECT * FROM `estudiantes`");
+    $sentence->execute();
+    $datos=$sentence->fetchAll();
+
+    Flight::json($datos);
+
 });
+
+
+
+
 
 Flight::start();
